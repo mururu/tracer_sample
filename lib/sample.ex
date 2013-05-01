a639890949e046ea35599258c982a525c4d571fa
Yuki Ito <yuki@gnnk.net>
2013-05-02 07:56:10 +0900

    fix script

--- lib/sample.ex
+++ lib/sample.ex
@@ -1,5 +1,5 @@
 defmodule Sample do
-  def sp do
+  def run do
     s = self
     spawn(fn()-> :timer.sleep(1000); s <- { :ok, 1 }  end)
     :timer.sleep(2000);
@@ -8,6 +8,18 @@ defmodule Sample do
     spawn(fn()-> :timer.sleep(1000); s <- { :ok, 3 }  end)
     :timer.sleep(2000);
     spawn(fn()-> :timer.sleep(1000); s <- { :ok, 4 }  end)
+    :timer.sleep(2000);
+    spawn(fn()-> :timer.sleep(1000); s <- { :ok, 5 }  end)
+    :timer.sleep(2000);
+    spawn(fn()-> :timer.sleep(1000); s <- { :ok, 6 }  end)
+    :timer.sleep(2000);
+    spawn(fn()-> :timer.sleep(1000); s <- { :ok, 7 }  end)
+    :timer.sleep(2000);
+    spawn(fn()-> :timer.sleep(1000); s <- { :ok, 8 }  end)
+    :timer.sleep(2000);
+    spawn(fn()-> :timer.sleep(1000); s <- { :ok, 9 }  end)
+    :timer.sleep(2000)
+    spawn(fn()-> :timer.sleep(1000); s <- :exit end)
     rec
   end
 
@@ -15,6 +27,8 @@ defmodule Sample do
     receive do
       { :ok , i } ->
         rec
+      :exit ->
+        IO.puts 'finish'
     end
   end
 end
